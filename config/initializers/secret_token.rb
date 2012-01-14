b3a87efae298c71053a416a069ee369c44786346
Jerel Miller <jermille@mines.edu>
2012-01-14 01:47:04 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EECS::Application.config.secret_token = '8f39cb27d0067389e95a7cde918925d4143144673b7d6b5f13b176f8ffef5bfa8439c3881949de94aaa199a96ea3e0e0f4fe96d41dc92d1e64ac24dcd6719fe5'
