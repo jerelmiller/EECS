72fbd350b9f86bf83388ecb7bf1846cd05c1054f
Jerel Miller <jermille@mines.edu>
2012-01-14 03:08:46 -0700

    Adding some layout stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EECS::Application.config.secret_token = '9b9b83a2b68c8962a127fcbf80a8b515f4858873cdd59d94df33ff90a25cde48910779d8f9d8f09ef17b1d780e9a410ac732fcf008f0e4fce1d9a8e5a5c00912'
